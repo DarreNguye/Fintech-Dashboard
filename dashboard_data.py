0149ea1257a2a607ca83773c1884b82246878c98
Darren Nguyen <darrenbanguyen@gmail.com>
2026-01-21 15:11:51 -0500

    Created dataframe with relevant stock information

---
+++ dashboard_data.py
@@ -0,0 +1,50 @@
+import yfinance as yf
+import pandas as pd
+import numpy as np
+
+def get_ticker_data(ticker, years):
+    '''
+    Fetch relevant data for a stock
+    Parameters:
+        ticker: Ticker representing a stock (str)
+        years: Amount of years to pull historical data (int)
+    Returns:
+        Price and fundamental data (dict)
+    '''
+    stock = yf.Ticker(ticker)
+
+    return {
+        'Ticker': ticker,
+        'Name': stock.info.get('shortName'),
+        'Sector': stock.info.get('sector'),
+        'Industry': stock.info.get('industry'),
+        'Market Cap': stock.info.get('marketCap'),
+        'Current Price': stock.info.get('currentPrice'),
+        'Change ($)': stock.info.get('regularMarketChange'),
+        'Change (%)': stock.info.get('regularMarketChangePercent'),
+        'Volume': stock.info.get('volume'),
+        '52-wk Range': stock.info.get('fiftyTwoWeekRange'),
+        'Prices': stock.history(period=f'{years}y')['Close'].tolist()
+    }
+
+def get_universe_data(tickers, years):
+    '''
+    Fetch relevant data for a universe of stocks
+    Parameters:
+        ticker: Tickers representing a stock (list of str)
+        years: Amount of years to pull historical data (int)
+    Returns:
+        Data for stocks (DataFrame)
+    '''
+
+    # Store Data
+    universe_data = []
+
+    # Pull Data for each Stock Individually
+    for ticker in tickers:
+        universe_data.append(get_ticker_data(ticker, years))
+    
+    # Convert to a DataFrame
+    universe_df = pd.DataFrame(universe_data)
+    universe_df.set_index('Ticker', inplace = True)
+    return universe_df
--- fintech_dashboard.py
+++ fintech_dashboard.py
@@ -1,57 +1,57 @@
 import streamlit as st
-import yfinance as yf
-import pandas as pd
-import numpy as np
+import dashboard_data as data
 
 # Page Setup
 st.title('Fintech Dashboard')
 
-# Sidebar for user input
-st.sidebar.header('Configuration')
-ticker = st.sidebar.text_input('Enter Ticker', value='NVDA').upper()
-lookback_years = st.sidebar.slider('Lookback Period (Years)', 1, 5, 3)
+# Styling
+def color_pos_or_neg(value):
+    color = '#09AB3B' if value >= 0 else 'red'
+    return f'color: {color};'
 
-# Fetch Data
-def get_data(ticker, years):
-    stock = yf.Ticker(ticker)
-    hist = stock.history(period=f'{years}y')
-    info = stock.info
-    return hist, info
+# Constants
+universe = ['NVDA', 'AAPL', 'MSFT', 'AMZN', 'META', 'V', 'LULU']
+lookback_years = 1
+universe_df = data.get_universe_data(universe, lookback_years)
+styled_universe_df = universe_df.style.map(color_pos_or_neg, subset=['Change (%)', 'Change ($)'])
 
-# Load data
-with st.spinner(f'Fetching data for {ticker}...'):
+# Configure Table
+column_configuration = {
+    'Market Cap': st.column_config.NumberColumn(
+        'Market Cap',
+        format = 'compact',
+    ),
+
+    'Change ($)': st.column_config.NumberColumn(
+        'Change ($)',
+        format = 'compact',
+    ),
+
+    'Change (%)': st.column_config.NumberColumn(
+        'Change (%)',
+        format = 'percent',
+    ),
+
+    'Volume': st.column_config.NumberColumn(
+        'Volume',
+        format = 'compact',
+    ),
+
+    'Prices': st.column_config.LineChartColumn(
+        f'Prices (Last {lookback_years} years)',
+        color = 'auto',
+    )
+}
+
+# Load Data Frame
+with st.spinner(f'Fetching data'):
     try:
-        df, info = get_data(ticker, lookback_years)
-        
-        # Metrics
-        current_price = df['Close'].iloc[-1]
-        prev_price = df['Close'].iloc[-2]
-        daily_change = (current_price - prev_price) / prev_price * 100
-        df['MA200'] = df['Close'].rolling(window=200).mean()
-        df['StdDev'] = df['Close'].rolling(window=200).std()
-        
-        # Z-Score = (Price - Average) / Standard Deviation
-        df['Z_Score'] = (df['Close'] - df['MA200']) / df['StdDev']
-        latest_z = df['Z_Score'].iloc[-1]
-        
-        # Visualizations
-
-        # Tickers
-        col1, col2, col3, col4 = st.columns(4)
-        col1.metric('Price', f"${current_price:.2f}", f"{daily_change:.2f}%")
-        col2.metric('P/E Ratio', f"{info.get('forwardPE', 'N/A'):.1f}")
-        col3.metric('Market Cap (in Billions)', f"${info.get('marketCap', 0)/1e9:.1f}")
-        col4.metric('Valuation Z-Score', f"{latest_z:.2f}", 
-                    help='Over 2.0 is expensive and under 2.0 is cheap')
-
-        # Charts
-        st.subheader(f'Price vs. 200-Day Average ({lookback_years} Years)')
-        st.line_chart(df[['Close', 'MA200']])
-
-        st.subheader('Valuation Heatmap (Z-Score)')
-        st.write('When this chart spikes high, the stock is statistically extended.')
-        st.bar_chart(df['Z_Score'])
+       st.dataframe(
+           styled_universe_df, 
+           column_config = column_configuration,
+           width = 'stretch'
+        )
 
     except Exception as e:
-        st.error(f'Error fetching data for {ticker}')
+        st.error(f'Error fetching data for {universe}')
         st.error(e)
